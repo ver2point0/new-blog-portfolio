276cc9bee0867ef9385c26c8cb142a4f88c9c805
John McIntosh II <jmci10@hotmail.com>
2016-12-03 21:33:42 +0000

    add show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,16 @@ class PostsController < ApplicationController
   
   def new 
   end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    
+    redirect_to @post
+  end
+  
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
