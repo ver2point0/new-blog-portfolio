3e299fcf51be41a1854fb43f61626b153ed299ee
John McIntosh II <jmci10@hotmail.com>
2016-12-06 09:38:24 +0000

    add about and portfolio pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   root "posts#index"
+  
   resources :posts do
     resources :comments
   end
   
+  get '/about', to: 'pages#about'
+  get '/portfolio', to: 'pages#portfolio'
 end
